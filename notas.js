377f84b6acc116cb882380cd33fb369c490f1609
toromaka <supe@mailfence.com>
2025-07-18 14:03:46 -0500

    Update notas.js

--- notas.js
+++ notas.js
@@ -3,31 +3,31 @@ const curso = urlParams.get("curso") || "Curso";
 document.getElementById("curso-nombre").textContent = curso;
 
 const notasPorCurso = {
-  "Bases de Datos II": [
+  "Bases_de_Datos_II": [
     { nombre: "Nota 1 (35%)", valor: 15, peso: 0.35 },
     { nombre: "Nota 2 (15%)", valor: 10, peso: 0.15 },
     { nombre: "Nota 3 (35%)", valor: 8, peso: 0.35 },
     { nombre: "Nota 4 (15%)", valor: 11, peso: 0.15 }
   ],
-  "IA para Negocios": [
+  "IA_para_Negocios": [
     { nombre: "Nota 1 (35%)", valor: 17, peso: 0.35 },
     { nombre: "Nota 2 (15%)", valor: 7, peso: 0.15 },
     { nombre: "Nota 3 (35%)", valor: 7, peso: 0.35 },
     { nombre: "Nota 4 (15%)", valor: 14, peso: 0.15 }
   ],
-  "Algoritmos Recursivos": [
+  "Algoritmos_Recursivos": [
     { nombre: "Nota 1 (35%)", valor: 13, peso: 0.35 },
     { nombre: "Nota 2 (15%)", valor: 13, peso: 0.15 },
     { nombre: "Nota 3 (35%)", valor: 13, peso: 0.35 },
     { nombre: "Nota 4 (15%)", valor: 11, peso: 0.15 }
   ],
-  "IA y Domótica": [
+  "IA_y_Domótica": [
     { nombre: "Nota 1 (35%)", valor: 13, peso: 0.35 },
     { nombre: "Nota 2 (15%)", valor: 11, peso: 0.15 },
     { nombre: "Nota 3 (35%)", valor: 13, peso: 0.35 },
     { nombre: "Nota 4 (15%)", valor: 12, peso: 0.15 }
   ],
-  "Seguridad de Datos": [
+  "Seguridad_de_Datos": [
     { nombre: "Nota 1 (35%)", valor: 13, peso: 0.35 },
     { nombre: "Nota 2 (15%)", valor: 13, peso: 0.15 },
     { nombre: "Nota 3 (35%)", valor: 12, peso: 0.35 },
